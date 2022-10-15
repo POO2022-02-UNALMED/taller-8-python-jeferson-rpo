ff973b96cc67476e77dde0301db40656a7aff8d2
jeferson-rpo <98125058+jeferson-rpo@users.noreply.github.com>
2022-10-15 10:25:45 -0500

    fut

--- futbolista.py
+++ futbolista.py
@@ -9,6 +9,20 @@ class Futbolista(Persona,Deportista):
         self.golesMarcados= golesMarcados
         self.tarjetasRojas= tarjetasRojas
         self.piernaHabil= piernaHabil
-        
+    
+    def setGolesMarcados(self,golesMarcados):
+        self.golesMarcados = golesMarcados
+    def getGolesMarcados(self):
+        return self.golesMarcados
+    def setTarjetasRojas(self,tarjetasRojas):
+        self.tarjetasRojas = tarjetasRojas
+    def getTarjetasRojas(self):
+        return self.piernaHabil
+    def setPiernaHabil(self,piernaHabil):
+        self.piernaHabil = piernaHabil
+    def getPiernaHabil(self):
+        return self.piernaHabil
+    
+
     def __str__(self):
         return "Mi nombre es ", Persona.getNombre(self), " soy profesional en el deporte ", Deportista.getDeporte(self)," Tengo ", Persona.getEdad(self)," anos de edad y llevo ",Deportista.getAñosPracticando(self)," años en el deporte”"  
